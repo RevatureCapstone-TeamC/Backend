c978b5727d142a6e507b70c14bfd8978902d45e0
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2023-01-04 14:56:11 -0800

    Add stock error handling

--- ECommerce.API/ECommerce.API/Controllers/ProductController.cs
+++ ECommerce.API/ECommerce.API/Controllers/ProductController.cs
@@ -51,29 +51,47 @@ namespace ECommerce.API.Controllers
         }
 
         [HttpPatch]
-        public async Task<ActionResult<Product[]>> Purchase(IEnumerable<Product> purchaseProducts)
+        public async Task<ActionResult<Product[]>> Purchase([FromBody] IEnumerable<Product> purchaseProducts)
         {
             /* _logger.LogInformation("PATCH api/product triggered"); */
             List<Product> products = new List<Product>();
+            Console.WriteLine(purchaseProducts.Count());
             try
             {
                 foreach (Product item in purchaseProducts)
                 {
-                    /* Product tmp = await _repo.GetProductByIdAsync(item.id); */
-                    var tmp = _context.Products.SingleOrDefault(p => 
+                    var tmp = _context.Products.SingleOrDefault(p =>
                         p.ProductName == item.ProductName);
-                    if ((tmp.ProductQuantity - 1) >= 0)
+                    if ((tmp.ProductQuantity - item.ProductQuantity) >= 0)
                     {
-                        /* await _repo.ReduceInventoryByIdAsync(item.ProductId, item.ProductQuantity); */
-                        tmp.ProductQuantity -= 1;
-                        await _context.SaveChangesAsync();
-                        /* products.Add(await _repo.GetProductByIdAsync(item.id)); */
-                        products.Add(await _context.Products.FindAsync(item.ProductId));
+                        //All Good - Making sure all items are in stock first
                     }
                     else
                     {
                         throw new Exception("Insuffecient inventory.");
                     }
+                    await _context.SaveChangesAsync();
+                }
+
+
+                foreach (Product item in purchaseProducts)
+                {
+                    /* Product tmp = await _repo.GetProductByIdAsync(item.id); */
+                    Console.WriteLine("=================================== {0}", item.ProductName);
+                    var tmp = _context.Products.SingleOrDefault(p =>
+                        p.ProductName == item.ProductName);
+                    if ((tmp.ProductQuantity - item.ProductQuantity) >= 0)
+                    {
+                        /* await _repo.ReduceInventoryByIdAsync(item.ProductId, item.ProductQuantity); */
+                        tmp.ProductQuantity -= item.ProductQuantity;
+                        /* products.Add(await _repo.GetProductByIdAsync(item.id)); */
+                        products.Add(await _context.Products.FindAsync(tmp.ProductId));
+                    }
+                    else
+                    {
+                        throw new Exception("Insuffecient inventory.");
+                    }
+                        await _context.SaveChangesAsync();
                 }
                 return Ok(products);
                 /* _logger.LogInformation("PATCH api/product completed successfully"); */
@@ -84,6 +102,40 @@ namespace ECommerce.API.Controllers
                 /* _logger.LogWarning("PATCH api/product completed with errors"); */
             }
         }
+        /*
+        public async Task<ActionResult<Product[]>> Purchase(IEnumerable<Product> purchaseProducts)
+        {
+
+            Console.WriteLine("------------------------ " + purchaseProducts.Count());
+            List<Product> products = new List<Product>();
+            try
+            {
+                Console.WriteLine("------------------------ Inside try block");
+                foreach (Product item in purchaseProducts)
+                {
+                    Console.WriteLine("------------------------ foreach " + item.ProductName, item.ProductId);
+                    var tmp = _context.Products.SingleOrDefault(p => 
+                        p.ProductName == item.ProductName);
+                    if ((tmp.ProductQuantity - 1) >= 0)
+                    {
+                        tmp.ProductQuantity -= 1;
+                        await _context.SaveChangesAsync();
+                        /* products.Add(await _repo.GetProductByIdAsync(item.id)); 
+                        products.Add(await _context.Products.FindAsync(item.ProductId));
+                    }
+                    else
+                    {
+                        throw new Exception("Insuffecient inventory.");
+                    }
+                }
+                return Ok(products);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+         */
 
     }
 }

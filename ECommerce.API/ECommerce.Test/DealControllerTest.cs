c6f2e06fd65391251aa67bd2f1de86eb654d1383
AkhilBh97 <akhil416@revature.net>
2023-01-04 07:50:15 -0800

    Done testing Deals

--- ECommerce.API/ECommerce.Test/DealControllerTest.cs
+++ ECommerce.API/ECommerce.Test/DealControllerTest.cs
@@ -71,80 +71,115 @@ public class DealControllerTest
         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
     }
 
+    // * CreateDeal returns Ok() if a Deal is successfully made
     [Fact]
     public async void CreateDealReturnsOk(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        Deal tmpD = new Deal{fk_Product_Id = 2, SalePrice=(decimal)250.0};
 
         // * ACT
+        var result = await controller.CreateDeal(tmpD);
+        _output.WriteLine($"CreateDeal() Returned: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result);
     }
 
+    // * UpdateDeal() returns NotFound() if a deal is not in the Deals DbSet
     [Fact]
     public async void UpdateDealReturnsNotFound(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        Deal tmpD = new Deal{DealId=4, fk_Product_Id=1, SalePrice=(decimal)0.99};
 
         // * ACT
+        var result = await controller.UpdateDeal(tmpD);
+        _output.WriteLine($"UpdateDeal() returns: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.NotFoundResult>(result);
     }
 
+    // * UpdateDeal() returns NoContent() if a deal is successfully updated
     [Fact]
     public async void UpdateDealReturnsNoContent(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        //let's grab a deal from the Context
+        var tmpD = (await controller.GetDeal(1)).Value;
+        //Now that the Deal is tracked, we can attempt to update it
+        tmpD.SalePrice=(decimal)300.50;
 
         // * ACT
+        var result = await controller.UpdateDeal(tmpD);
+        _output.WriteLine($"UpdateDeal() returns: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.NoContentResult>(result);
     }
 
+    // * DeleteDEal(id) returns NotFound() if no deal exists
     [Fact]
     public async void DeleteDealIDReturnsNotFound(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        //only 2 Deals in the Context, let's try deleting #5
+        int id = 5;
 
         // * ACT
+        var result = await controller.DeleteDeal(id);
+        _output.WriteLine($"DeleteDeal() returns: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.NotFoundResult>(result);
     }
 
+    // * DeleteDeal(id) returns NoContent() if a deal is deleted
     [Fact]
     public async void DeleteDealIDReturnsNoContent(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        int id = 2;
 
         // * ACT
+        var result = await controller.DeleteDeal(id);
+        _output.WriteLine($"DeleteDeal(id) returns: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.NoContentResult>(result);
     }
 
+    // * DeleteDeals() returns NoContent() if all deals are cleared
+    // * DeleteDeals() means a list of deals returned by GetDeals() is empty
     [Fact]
     public async void DeleteDealsReturnsNoContent(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
 
         // * ACT
+        var result = await controller.DeleteDeals();
+        var getres = await controller.GetDeals();
+        _output.WriteLine($"DeleteDeals() returns: {result}\nGetDeals on Empty Deals returns: {getres}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.NoContentResult>(result);
+        Assert.Empty(getres.Value);
     }
 
+    // * DeleteDeals() returns BadRequest(obj message) if there are no Deals to delete
     [Fact]
     public async void DeleteDealsReturnsBadRequest(){
-        // TODO : IMPLEMENT
         // * ARRANGE
         var controller = new DealController(_fixture.Context);
+        //first let's clear the deals out
+        await controller.DeleteDeals();
 
         // * ACT
+        var result = await controller.DeleteDeals();
+        _output.WriteLine($"DeleteDeals() returns: {result}");
 
         // * ASSERT
+        Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
     }
 }

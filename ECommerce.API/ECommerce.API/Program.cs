e76e45d15bc8727c74b1609f92dfaa07fa74acb2
MasonCathorall <masoncathorallrev@gmail.com>
2023-01-06 15:16:56 -0500

    using swagger

--- ECommerce.API/ECommerce.API/Program.cs
+++ ECommerce.API/ECommerce.API/Program.cs
@@ -51,14 +51,11 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
-//app.UseSwagger();
-//app.UseSwaggerUI();
+//}
 
 app.UseCors(ECommerceAPI);
 
